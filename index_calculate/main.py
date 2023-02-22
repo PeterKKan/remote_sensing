d69ef0f455c498e951d974061760ef810f28ff79
PeterKKan <49744148+PeterKKan@users.noreply.github.com>
2023-02-22 11:55:05 +0800

    Add files via upload

---
+++ index_calculate/main.py
@@ -0,0 +1,123 @@
+import numpy as np
+from pyhdf.SD import SD, SDC
+from osgeo import gdal
+from osgeo import osr
+import os
+
+HDF_DIR_PATH = "D:\\study\\yanyi\\yaogan\\week2\\MOD09A1"  # path of hdf file directory
+OUTPUT_PATH = ""  # path of the output directory
+R = "sur_refl_b01"  # band name and its corresponding SDS name
+NIR = "sur_refl_b02"
+B = "sur_refl_b03"
+SWIR = "sur_refl_b06"
+
+
+def main():
+    calculate_index(get_hdf_files())
+
+
+def get_hdf_files():
+    """
+    read hdf files from a specified directory and
+    convert them into python hdf objects.
+
+    :param:
+      none.
+
+    :return hdf_file_object_dict:
+      a dictionary:
+        keys: hdf file name without ".hdf"
+        values: corresponding python hdf object
+    """
+
+    hdf_file_name_list = os.listdir(HDF_DIR_PATH)
+    hdf_file_object_dict = {}
+    for file_name in hdf_file_name_list:
+        hdf_file_object_dict[file_name[:-4]] = SD(HDF_DIR_PATH + "\\" + file_name, SDC.READ)
+    return hdf_file_object_dict
+
+
+def calculate_index(hdf_file_object_dict):
+    """
+    calculate NDVI, EVI, EVI2, LSWI of each python hdf object and
+    save the result to a specified directory.
+
+    :param hdf_file_object_dict:
+      a dictionary:
+        keys: hdf file name without ".hdf"
+        values: corresponding python hdf object
+
+    :return:
+      none.
+    """
+
+    for hdf_file_name, hdf_object in hdf_file_object_dict.items():
+        r = get_band_data_matrix(hdf_object, R)
+        n = get_band_data_matrix(hdf_object, NIR)
+        b = get_band_data_matrix(hdf_object, B)
+        swir = get_band_data_matrix(hdf_object, SWIR)
+
+        ndvi = (n - r) / (n + r)
+        evi = 2.5 * (n - r) / (n + 6 * r - 7.5 * b + 1)
+        evi2 = 2.5 * (n - r) / (n + 2.4 * r + 1)
+        lswi = (n - swir) / (n + swir)
+
+        save_as_tiff(ndvi, "NDVI", hdf_file_name)
+        save_as_tiff(evi, "EVI", hdf_file_name)
+        save_as_tiff(evi2, "EVI2", hdf_file_name)
+        save_as_tiff(lswi, "LSWI", hdf_file_name)
+
+
+def get_band_data_matrix(hdf_object, band):
+    """
+    get data of a band, and convert it into numpy matrix.
+
+    :param hdf_object:
+      the python hdf object.
+
+    :param band:
+      which band's data need to convert.
+      options:
+         R(red)/NIR/B(blue)/SWIR
+
+    :return:
+      a numpy matrix of data of a band.
+    """
+    band_object = hdf_object.select(band)
+    band_data = band_object.get()
+    return np.matrix(band_data)
+
+
+def save_as_tiff(matrix, index_type, hdf_file_name):
+    """
+    save the calculated matrix as a tiff file.
+
+    :param matrix:
+      matrix that needs to be written to the tiff file.
+
+    :param index_type:
+      the index type.
+
+    :param hdf_file_name:
+      hdf file name without ".hdf"
+
+    :return:
+      none.
+    """
+
+    x_pixels = matrix.shape[1]  # number of pixels in x
+    y_pixels = matrix.shape[0]  # number of pixels in y
+    driver = gdal.GetDriverByName('GTiff')
+
+    output_path = ""
+    if OUTPUT_PATH == "":
+        output_path = HDF_DIR_PATH + "\\" + index_type
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    output_file_name = output_path + "\\" + hdf_file_name + index_type + ".tiff"
+    created_temp = driver.Create(output_file_name, x_pixels, y_pixels, 1, gdal.GDT_Float32)
+    created_temp.GetRasterBand(1).WriteArray(matrix)
+
+
+if __name__ == '__main__':
+    main()
---
+++ index_calculate/readme.txt
@@ -0,0 +1,2 @@
+运行main.py之前，先按实际情况修改第7行的HDF_DIR_PATH参数，修改为包含hdf文件的文件夹。
+还可以修改第8行的OUTPUT_PATH参数，此参数规定了输出的tiff的位置，如果不修改，默认会将tiff文件输出到HDF_DIR_PATH目录下。
